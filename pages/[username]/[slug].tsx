ef709debf92c0e0e3f9a3154dee675ecf51ad160
abrarfahim19 <fahimpatwari19@gmail.com>
2022-08-18 13:21:07 -0400

    Posting

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,10 +1,96 @@
-import Link from "next/link";
+import styles from '@styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+// import HeartButton from '@components/HeartButton';
+// import AuthCheck from '@components/AuthCheck';
+// import Metatags from '@components/Metatags';
+import { UserContext } from '../../lib/context';
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 
-export default function SlugPost({}) {
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useContext } from 'react';
+import Link from "next/link";
+import { collection, doc } from 'firebase/firestore';
+
+export async function getStaticProps({ params }) {
+    const { username, slug } = params;
+    const userDoc = await getUserWithUsername(username);
+  
+    let post;
+    let path;
+  
+    if (userDoc) {
+    //   const postRef = userDoc.ref.collection('posts').doc(slug);
+      const postRef = collection(userDoc.ref, "posts").doc(slug)
+      post = postToJSON(await postRef.get());
+  
+      path = postRef.path;
+    }
+  
+    return {
+      props: { post, path },
+      revalidate: 100,
+    };
+  }
+  
+  export async function getStaticPaths() {
+    // Improve my using Admin SDK to select empty docs
+    const snapshot = await firestore.collectionGroup('posts').get();
+  
+    const paths = snapshot.docs.map((doc) => {
+      const { slug, username } = doc.data();
+      return {
+        params: { username, slug },
+      };
+    });
+  
+    return {
+      // must be in this format:
+      // paths: [
+      //   { params: { username, slug }}
+      // ],
+      paths,
+      fallback: 'blocking',
+    };
+  }
+  
+  export default function Post(props) {
+    const postRef = firestore.doc(props.path);
+    const [realtimePost] = useDocumentData(postRef);
+  
+    const post = realtimePost || props.post;
+  
+    const { user: currentUser } = useContext(UserContext);
+  
     return (
-        <main>
-            <h2>This is Slug Post</h2>
-            <Link style={{color:"red", marginTop:"100px"}} prefetch href={"/enter"}>Go to entry Page instead</Link>
-        </main>
-    )
-}
+      <main className={styles.container}>
+        {/* <Metatags title={post.title} description={post.title} /> */}
+        
+        <section>
+          <PostContent post={post} />
+        </section>
+  
+        <aside className="card">
+          <p>
+            <strong>{post.heartCount || 0} 🤍</strong>
+          </p>
+  
+          {/* <AuthCheck
+            fallback={
+              <Link href="/enter">
+                <button>💗 Sign Up</button>
+              </Link>
+            }
+          >
+            <HeartButton postRef={postRef} />
+          </AuthCheck> */}
+  
+          {currentUser?.uid === post.uid && (
+            <Link href={`/admin/${post.slug}`}>
+              <button className="btn-blue">Edit Post</button>
+            </Link>
+          )}
+        </aside>
+      </main>
+    );
+  }
+  
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,68 @@
-import toast from "react-hot-toast";
-import Loader from "../components/Loader";
-import styles from "../styles/Home.module.css";
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
-export default function Home() {
+import { useState } from 'react';
+import { collection, collectionGroup, getDocs, limit, orderBy, query, startAfter, where } from 'firebase/firestore';
+
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postsQuery = query(collectionGroup(firestore, "posts"),where('published', '==', true), orderBy('createdAt', 'desc'), limit(LIMIT));
+  // const postsQu = firestore
+  //   .collectionGroup('posts')
+  //   .where('published', '==', true)
+  //   .orderBy('createdAt', 'desc')
+  //   .limit(LIMIT);
+
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const newPostquery = query(collectionGroup(firestore, "posts"),where('published', '==', true), orderBy('createdAt', 'desc'), limit(LIMIT), startAfter(cursor));
+
+    const newPosts = (await getDocs(newPostquery)).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <Loader show/>
-      <button onClick={ ()=> toast.success("I am successfully Landing...!")}>Toast Me!</button>
-    </div>
+    <main>
+      <div className="card card-info">
+        <h2>💡 Next.js + Firebase - The Full Course</h2>
+        <p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
+        <p>Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine optimized.</p>
+      </div>
+     {/* @ts-ignore */}
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
   );
 }
